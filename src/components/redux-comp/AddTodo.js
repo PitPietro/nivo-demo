ae3d44cd5f0cc9d73df28905c02f3378004bce6a
Pietro Poluzzi <pietropoluzzi@MacBook-Pro.home>
2021-03-03 12:56:10 +0100

    key binding on Enter a TodoItem

--- src/components/redux-comp/AddTodo.js
+++ src/components/redux-comp/AddTodo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../../redux/actions";
+import {Button} from "react-bootstrap";
+
+
 
 class AddTodo extends React.Component {
     constructor(props) {
         super(props);
         this.state = { input: "" };
+
+        this.keyEnter = this.keyEnter.bind(this);
     }
 
     updateInput = input => {
@@ -17,16 +22,28 @@ class AddTodo extends React.Component {
         this.setState({ input: "" });
     };
 
+    // very useful
+    // https://stackoverflow.com/questions/35862979/how-to-detect-which-react-component-triggers-onkeyup-event
+
+    // https://codepen.io/Audiosyncrasy/pen/pQdpbJ
+
+    keyEnter(event) {
+        if (event.key === "Enter") {
+            this.handleAddTodo();
+        }
+    }
+
     render() {
         return (
             <div>
                 <input
                     onChange={e => this.updateInput(e.target.value)}
+                    onKeyPress={this.keyEnter}
                     value={this.state.input}
                 />
-                <button className="add-todo" onClick={this.handleAddTodo}>
+                <Button variant="success" className="add-todo" onClick={this.handleAddTodo}>
                     Add Todo
-                </button>
+                </Button>
             </div>
         );
     }
